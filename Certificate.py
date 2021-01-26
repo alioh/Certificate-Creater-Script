9246a1e347f7d485d6a508e735b143b0ac3b38be
Ali O <Ali.alohali@gmail.com>
2021-01-26 15:35:20 +0300

    files

---
+++ Certificate.py
@@ -0,0 +1,40 @@
+# reference: https://haptik.ai/tech/putting-text-on-image-using-python/
+
+from PIL import Image, ImageDraw, ImageFont
+import arabic_reshaper
+from bidi.algorithm import get_display
+import os
+
+#   Load preferable font
+font = ImageFont.truetype('calibri.ttf', size=150)
+
+#   Certificate size
+(x, y) = (3508, 2481)
+
+#   Output color
+color = 'rgb(73, 85, 125)'
+
+#   Loop through names and write/save
+def createCertificate(workshop, attendees_list):
+    for i in attendees_list:
+        img = Image.open('Certificate.png')
+        draw = ImageDraw.Draw(img)
+
+        #   Size of text
+        w, h = draw.textsize(i, font)
+
+        #   https://gist.github.com/amrza/04658c71ac02d82580855f89b9b3dff4
+        reshaped_attendees_name = arabic_reshaper.reshape(i)
+        text_attendees_name = get_display(reshaped_attendees_name)
+        draw.text((((x/2)-(w/2.5)), ((y - h) / 2)-310), text_attendees_name, fill=color, font=font)
+
+        p, q = draw.textsize(workshop, font)
+
+        reshaped_workshop = arabic_reshaper.reshape(workshop)
+        workshop_name = get_display(reshaped_workshop)
+        draw.text((((x/2)-(p/2.5)), ((y - q) / 2)+50), workshop_name, fill=color, font=font)
+
+        img.save(f'{i}_certificate.png')
+
+att = ["أسم المتدرب الأول", "أسم المتدرب الثاني"]
+createCertificate("عنوان لورشة العمل", att)
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Certificate-Creater-Script
+
+## How it work?
+
+- Design your certificate in the ppt file.
+- Make sure the certificate dimensions are: 3508, 2481 pixels. 36.54, 25.84 in.
+- The text in the middle should not be moved otherwise you have to do changes to where you want to put the attendees and event name. You can change that in line 29 for attendees name and line 35 for event name. The function draw.text() is the one that write, so you need to do your changes in its first parameter.
+- Use the function createCertificate to create your certificates. It takes:
+    - Event name.
+    - List of attendees.
+- Run the code.
+- You will get your certificates in this shape:
+    - [attendees name]_certificate.png
+
